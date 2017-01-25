7b957ff270bb5c6abfb668d41f29cd2c80d856a1
Jamie Graham <jamie@domain7.com>
2017-01-25 15:41:51 -0800

    Adds Oauth signin with Google, Facebook, Twitter, using Firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react'
-import { login } from '../helpers/auth'
+import { login, loginOauth } from '../helpers/auth'
 
 export default class Login extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     login(this.email.value, this.pw.value)
   }
+  handleClickOauth = (e) => {
+    e.preventDefault();
+    loginOauth(e.target.id);
+  }
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input className="" ref={(email) => this.email = email} placeholder="Email"/>
-        <input type="password" placeholder="Password" ref={(pw) => this.pw = pw} />
-        <button type="submit">Login</button>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input className="" ref={(email) => this.email = email} placeholder="Email"/>
+          <input type="password" placeholder="Password" ref={(pw) => this.pw = pw} />
+          <button type="submit">Login</button>
+        </form>
+        <button id="google" href="#" className="" onClick={this.handleClickOauth}>Login with Google</button>
+        <button id="facebook" href="#" className="" onClick={this.handleClickOauth}>Login with facebook</button>
+        <button id="twitter" href="#" className="" onClick={this.handleClickOauth}>Login with twitter</button>
+
+      </div>
     )
   }
 }
--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -14,6 +14,33 @@ export function login (email,pw) {
   return firebaseAuth().signInWithEmailAndPassword(email, pw)
 }
 
+export function loginOauth(oauthId) {
+
+  switch(oauthId) {
+    case 'google':
+      var provider = new firebaseAuth.GoogleAuthProvider();
+      loginPopup(provider);
+      break;
+    case 'facebook':
+      provider = new firebaseAuth.FacebookAuthProvider();
+      loginPopup(provider);
+      console.log('facebook')
+      break;
+    case 'twitter':
+      provider = new firebaseAuth.TwitterAuthProvider();
+      loginPopup(provider);
+      break;
+  }
+}
+
+export function loginPopup(provider){
+  return firebaseAuth().signInWithPopup(provider).then(function(result) {
+    console.log('logged in')
+  }).catch(function(error) {
+    console.log(error)
+  });    
+}
+
 export function saveUser(user) {
   return ref.child('users/${user.uid}/info')
     .set({
